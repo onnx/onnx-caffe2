f5be874856bbb86e4372a8feabebdd2cd9a1548c
Junjie Bai <bai@in.tum.de>
2017-09-07 16:07:31 -0700

    Fix enum dependency #1
    enum is builtin starting from Python 3.4

--- setup.py
+++ setup.py
@@ -3,14 +3,18 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-
+import sys
 from setuptools import setup, find_packages
+
+install_requires = []
+if sys.version_info < (3, 4):
+    install_requires.append('enum34')
 
 setup(
     name="onnx-caffe2",
-    version='0.1.1',
+    version='0.1.2',
     description="Caffe2 frontend and backend of Open Neural Network Exchange",
-    install_requires=['enum'],
+    install_requires=install_requires,
     setup_requires=['pytest-runner'],
     tests_require=['numpy', 'pytest-cov'],
     packages=find_packages(),
