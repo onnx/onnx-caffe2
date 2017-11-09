6a3664fff6bd1411adae77adc8285503bbcecdf5
Junjie Bai <bai@in.tum.de>
2017-11-09 02:36:52 -0800

    Use new onnx backend test features to enable report and exclude vgg19 on travis

--- tests/onnx_backend_test.py
+++ tests/onnx_backend_test.py
@@ -3,14 +3,23 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import unittest
+import os
 
+import unittest
 import onnx.backend.test
 
 import onnx_caffe2.backend as c2
 
+# This is a pytest magic variable to load extra plugins
+pytest_plugins = 'onnx.backend.test.report',
+
+backend_test = onnx.backend.test.BackendTest(c2, __name__)
+if 'TRAVIS' in os.environ:
+    backend_test.exclude('test_vgg19')
 # import all test cases at global scope to make them visible to python.unittest
-globals().update(onnx.backend.test.BackendTest(c2.Caffe2Backend, __name__).test_cases)
+globals().update(backend_test
+                 .enable_report()
+                 .test_cases)
 
 if __name__ == '__main__':
     unittest.main()
