cbc4c1727c85d5e5284ed7bafa0670fbfd96ad61
Lu Fang <lufang@fb.com>
2018-01-24 12:13:02 -0800

    Remove onnx-optimize completely

--- setup.py
+++ setup.py
@@ -58,33 +58,19 @@ class build_py(setuptools.command.build_py.build_py):
         self.run_command('create_version')
         setuptools.command.build_py.build_py.run(self)
 
-
-def build_optimize_onnx():
-    optimize_onnx_dir = os.path.join('build', 'optimize-onnx')
-    subprocess.check_call(['mkdir', '-p', optimize_onnx_dir])
-    subprocess.check_call(['cmake', os.path.join('..', '..', 'optimize-onnx')], cwd=optimize_onnx_dir)
-    subprocess.check_call(['make'], cwd=optimize_onnx_dir)
-
 class build(distutils.command.build.build):
     def run(self):
         self.run_command('build_py')
-        build_optimize_onnx()
 
 class develop(setuptools.command.develop.develop):
     def run(self):
         self.run_command('create_version')
         self.run_command('build')
         setuptools.command.develop.develop.run(self)
-        source = os.path.join(TOP_DIR, 'build', 'optimize-onnx', 'src', 'optimize-onnx')
-        target = os.path.join(sys.prefix, 'bin', '')
-        subprocess.check_call(['ln', '-s', '-f', source, target])
 
 class install(setuptools.command.install.install):
     def run(self):
         setuptools.command.install.install.run(self)
-        source = os.path.join(TOP_DIR, 'build', 'optimize-onnx', 'src', 'optimize-onnx')
-        target = os.path.join(sys.prefix, 'bin', '')
-        subprocess.check_call(['cp', source, target])
 
 cmdclass={
     'create_version': create_version,
