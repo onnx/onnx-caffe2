6369f8d273192e51d7d346a251f2d726665a6aef
bddppq <bai@in.tum.de>
2018-01-24 21:27:47 -0800

    Minor fix to test.sh

--- test.sh
+++ test.sh
@@ -23,8 +23,7 @@ do
 done
 set -- "${UNKNOWN[@]}" # leave UNKNOWN
 
-script_path=$(python -c "import os; import sys; print(os.path.realpath(sys.argv[1]))" "${BASH_SOURCE[0
-]}")
+script_path=$(python -c "import os; import sys; print(os.path.realpath(sys.argv[1]))" "${BASH_SOURCE[0]}")
 top_dir=$(dirname "$script_path")
 TEST_DIR="$top_dir/tests"
 
